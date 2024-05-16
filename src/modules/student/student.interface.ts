9a98b59ec54ab9e2a88a47a011a650648d71389f
nobabsarkar <nobabsarkar2016@gmail.com>
2024-05-16 11:10:51 +0300

    change file name

---
+++ src/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type LocalGuargian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  contactNo: string;
+  emergencyNo: string;
+  bloodBroup?: 'A+ ' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuargian;
+  profileImage?: string;
+  isActive: 'active' | 'inActive';
+};
